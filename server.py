6b91c07ac792261697c9c139e8daf4906db73bc4
Rauzat Ahnaf Amin <rauzatahnaf@gmail.com>
2025-10-24 21:01:51 +0800

    Updated server.py with [brief description of change]

--- server.py
+++ server.py
@@ -116,6 +116,9 @@ def get_audio_file(filename):
 
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
